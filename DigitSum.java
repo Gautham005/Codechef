82e3bbffbcb806549e5fea8a50168320455d0141
Your Name <you@example.com>
2017-09-11 20:37:57 +0530

    Sum of digits problem

---
+++ DigitSum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class DigitSum{
+	public static void main(String[] args) {
+		int sum,a,r,n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			sum=0;
+			a=s.nextInt();
+			while(a!=0)
+		{
+			r=a%10;
+			sum+=r;
+			a=a/10;
+		}
+		System.out.println(sum);
+		}
+
+	}
+}
